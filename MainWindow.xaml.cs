e631327e739e11a67f60bd93346ed62e117d7216
Lazaro Padron <lazaropadron@gmail.com>
2016-03-22 12:35:07 -0400

    Laz's original updates to the build
    This contains the SQL class and GUI updates for our tracker.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Forms;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
-namespace eSportsBadgeTracker
-{
+namespace eSportsBadgeTracker {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : MetroWindow {
+        public int selectedIndex = -1;
+        public string selectedCustomerID;
+        public SearchUser searchPage;
+
+        public MainWindow() {
             InitializeComponent();
-            LogInPrompt mainpage = new LogInPrompt();
-            mainFrame.Navigate(mainpage);
+            searchPage = new SearchUser();
+            /*
+            NavigationWindow navWin = new NavigationWindow();
+            navWin.Content = searchPage;
+            navWin.Show();
+            */
         }
 
+        private void btnCheckIn_Click(object sender, RoutedEventArgs e) {
+            //searchPage = new SearchUser();
+        }
 
+        private void btnRedeem_Click(object sender, RoutedEventArgs e) {
+            RedeemPage redeemPage = new RedeemPage();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e) {
+            RegistrationPage rp = new RegistrationPage();
+            rp.searchPage = searchPage;
+        }
     }
 }
